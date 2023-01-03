00bcb2f95093f647b11fb7c77f798b60c59cebff
GrayWolf64 <88234821+GrayWolf64@users.noreply.github.com>
2023-01-03 08:56:40 +0800

    Added x labels.

--- lua/gplotlib/figure.lua
+++ lua/gplotlib/figure.lua
@@ -2,7 +2,6 @@ local figure = {}
 
 function figure.setup(panel, w, h, color_bg, color_outline)
     local base_panel = vgui.Create("DPanel", panel)
-    base_panel:Dock(FILL)
     base_panel:SetSize(w, h)
     base_panel:Center()
 
@@ -18,11 +17,11 @@ end
 
 local axis = {}
 
-function axis.setup(fig, space_x, space_y, length_x, length_y)
+function axis.setup(fig, space_x, space_y, length_x, length_y, color_axis)
     local layer_axis = vgui.Create("DPanel", fig)
     layer_axis:Dock(FILL)
     local num_spaces_x, num_spaces_y = math.floor(fig:GetWide() / space_x), math.floor(fig:GetTall() / space_y)
-    surface.SetDrawColor(color_black)
+    surface.SetDrawColor(color_axis)
 
     function layer_axis.Paint(self, w, h)
         for i = 1, num_spaces_x do
@@ -35,15 +34,25 @@ function axis.setup(fig, space_x, space_y, length_x, length_y)
             surface.DrawLine(0, start_end_y, length_y, start_end_y)
         end
     end
+
+    local fig_x, fig_y = fig:GetPos()
+    local layer_label = vgui.Create("DPanel", fig:GetParent())
+    layer_label:Dock(FILL)
+
+    function layer_label.Paint()
+        for i = 0, num_spaces_x do
+            draw.SimpleText(tostring(i * space_x), "DermaDefault", fig_x + i * space_x, fig:GetTall() + layer_label:GetY(), color_white)
+        end
+    end
 end
 
 local frame = nil
 frame = vgui.Create("DFrame")
-frame:SetSize(400, 400)
+frame:SetSize(500, 500)
 frame:SetTitle("figure")
 frame:SetVisible(true)
 frame:ShowCloseButton(true)
 frame:MakePopup()
 frame:Center()
-local fig = figure.setup(frame, 380, 380, Color(204, 204, 204), Color(68, 68, 68))
-axis.setup(fig, 10, 10, 5, 5)
+local fig = figure.setup(frame, 400, 400, Color(204, 204, 204), Color(68, 68, 68))
+axis.setup(fig, 40, 40, 5, 5, color_black)
