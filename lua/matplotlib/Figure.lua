3ef41b7f93fd0fbf4ff4711e14114738ae28f7da
GrayWolf64 <88234821+GrayWolf64@users.noreply.github.com>
2023-01-15 20:00:10 +0800

    Figure.Point.

--- lua/matplotlib/Figure.lua
+++ lua/matplotlib/Figure.lua
@@ -1,21 +1,25 @@
 local Figure = {}
 
-function Figure.Setup(panel, x, y, w, h, ColorBG, ColorOutline, ColorAxes)
+function Figure.Setup(panel, x, y, w, h, colorbg, coloroutline, coloraxes)
     local BasePanel = vgui.Create("DPanel", panel)
     BasePanel:SetSize(w, h)
     BasePanel:SetPos(x, y)
 
     function BasePanel.Paint()
-        surface.SetDrawColor(ColorBG)
+        surface.SetDrawColor(colorbg)
         BasePanel:DrawFilledRect()
-        surface.SetDrawColor(ColorOutline)
+        surface.SetDrawColor(coloroutline)
         BasePanel:DrawOutlinedRect()
-        surface.SetDrawColor(ColorAxes)
+        surface.SetDrawColor(coloraxes)
         surface.DrawLine(0, h / 2, w, h / 2)
         surface.DrawLine(w / 2, 0, w / 2, h)
     end
 
     return BasePanel
+end
+
+function Figure.Point(panel, x, y, color)
+    surface.DrawCircle(x, y, 1, color)
 end
 
 local Frame = nil
